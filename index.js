f0690cee59bcea425f3b55ae466614c6b82e5b44
Ryan Bravo <rrbravo2@gmail.com>
2019-04-21 05:57:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
