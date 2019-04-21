d4c6175aacf1c830bce13fe0f540eb52d321d56e
Ryan Bravo <rrbravo2@gmail.com>
2019-04-21 05:58:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // add solution here
-
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
+}
 
 function johnLennonFacts(facts) {
   var results = [];
